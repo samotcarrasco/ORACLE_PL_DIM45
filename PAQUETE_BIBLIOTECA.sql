2b5b4bee0e5d41334261c047b091eb13a10d3eab
samotcarrasco <samotcarrasco@gmail.com>
2023-03-10 10:56:38 +0100

    hasta el 12

--- PAQUETE_BIBLIOTECA.sql
+++ PAQUETE_BIBLIOTECA.sql
@@ -1,6 +1,10 @@
 CREATE OR REPLACE PACKAGE BiblioTK AS
   FUNCTION alta_obra(p_titulo VARCHAR2, p_año INTEGER DEFAULT NULL) RETURN VARCHAR2;
   FUNCTION borrado_obra(p_id VARCHAR) RETURN INTEGER;
+  FUNCTION alta_autor(p_nombre VARCHAR, p_apellidos VARCHAR, p_nacimiento DATE DEFAULT NULL) RETURN VARCHAR2;
+  FUNCTION borrado_autor(p_id VARCHAR) RETURN INTEGER;
+  FUNCTION vincular(p_id_autor VARCHAR, p_id_obra VARCHAR) RETURN INTEGER;
+
 END BiblioTK;
 /
 
@@ -16,9 +20,13 @@ FUNCTION alta_obra(p_titulo VARCHAR2, p_año INTEGER DEFAULT NULL)
     v_id CHAR(5);
  BEGIN
     v_id := dbms_random.string('X', 5);
-    INSERT INTO obra (id, titulo) VALUES (v_id, p_titulo);
-    -- Devolver el ID de la nueva obra
-    RETURN v_id;
+    INSERT INTO obra (id, titulo, año) VALUES (v_id, p_titulo, p_año);
+    -- si el insert no devuelve nada devolvemos null, si no, devolvemos el id
+    IF SQL%FOUND THEN
+        RETURN v_id;
+    ELSE 
+        RETURN null;
+    END IF;
  END;
  
 
@@ -34,7 +42,7 @@ FUNCTION borrado_obra(p_id VARCHAR)
     v_resultado := 0;
     -- Comprobar si existe la obra con el ID proporcionado
     SELECT COUNT(*) INTO v_resultado FROM obra WHERE id = p_id;
-    -- Si se encuentra la obra, borrarla
+    -- Si hay 1 o mas los borra(deberia haber solo 0 o 1 porque el PK)
     IF v_resultado > 0 THEN
         DELETE FROM obra WHERE id = p_id;
         v_resultado := 1;
@@ -44,8 +52,67 @@ FUNCTION borrado_obra(p_id VARCHAR)
     RETURN v_resultado;
  END;
 
+-- 10. El alta de autores se implementará mediante una función alta_autor.
+ -------------------------------------------------------------------------------
+-- efectos: alta de autor con nombre, apellidos y nacimiento
+-- retorno: id asignado
+-------------------------------------------------------------------------------
+FUNCTION alta_autor(p_nombre VARCHAR, p_apellidos VARCHAR, p_nacimiento DATE DEFAULT NULL)
+ RETURN VARCHAR2 IS 
+    v_id CHAR(4);
+BEGIN
+    -- segun la definición de la tabla,los id de los autores don de 4 caracteres
+    v_id := dbms_random.string('X', 4);
+    INSERT INTO autor (id, nombre, apellidos, nacimiento) VALUES (v_id, p_nombre, p_apellidos, p_nacimiento);
+     -- si el insert no devuelve nada devolvemos null, si no, devolvemos el id
+    IF SQL%FOUND THEN
+        RETURN v_id;
+    ELSE 
+        RETURN null;
+    END IF;
+END;
+
+--11. El borrado de autores se implementará mediante una función borrado_autor.
+-------------------------------------------------------------------------------
+-- efectos: borrado de autor por id
+-- retorno: 1 si borrado efectuado, 0 si no existe id
+-------------------------------------------------------------------------------
+FUNCTION borrado_autor(p_id VARCHAR)
+ RETURN INTEGER IS
+  v_resultado INTEGER;
+ BEGIN
+    v_resultado := 0;
+    -- Comprobar si existe el autor con el ID proporcionado
+    SELECT COUNT(*) INTO v_resultado FROM autor WHERE id = p_id;
+    -- Si hay 1 o mas los borra(deberia haber solo 0 o 1 porque el PK)º
+    IF v_resultado > 0 THEN
+        DELETE FROM autor WHERE id = p_id;
+        v_resultado := 1;
+    END IF;
+    
+    -- Devolver el resultado del borrado (1 si se borra, 0 si no se encuentra el ID)
+    RETURN v_resultado;
+ END;
  
- END BiblioTK;
+ 
+ --12. La vinculación de un autor a una obra se implementará mediante una función vincular.
+-------------------------------------------------------------------------------
+-- efectos: vincula un autor a una obra
+-- retorno: 1 (siempre)
+-------------------------------------------------------------------------------
+FUNCTION vincular(p_id_autor VARCHAR, p_id_obra VARCHAR)
+ RETURN INTEGER IS
+ v_resultado INTEGER := 0;
+BEGIN
+  INSERT INTO autor_obra (id_autor, id_obra) VALUES (p_id_autor, p_id_obra);
+  IF SQL%FOUND THEN
+    v_resultado := 1;
+  END IF;
+  RETURN v_resultado;
+END;
+
+END BiblioTK;
+ 
  /
  
 
--- comprobacion.sql
+++ comprobacion.sql
@@ -3,14 +3,18 @@
 DECLARE
   v_id CHAR(5);
 BEGIN   
-  v_id := bibliotk.alta_obra('Historias del DIM45', 2022);
-  dbms_output.put_line('Obra dada de ALTA: ' || v_id);
+  v_id := bibliotk.alta_obra('Historias del DIM45', '2023');
+
+  IF v_id IS NOT NULL THEN
+    dbms_output.put_line('La obra '||v_id||' se ha dado de alta correctamente');
+  ELSE
+    dbms_output.put_line('La obra no se ha dado de alta');
+  END IF;
+  
 END;
 /
 
-
 select * from obra;
-
 
 -- ejercicio 9
 DECLARE
@@ -25,4 +29,38 @@ BEGIN
 END;
 /
 
+
+-- ejercicio 10
+DECLARE
+  v_id CHAR(5);
+BEGIN   
+  v_id := bibliotk.alta_autor('TOMAS', 'PEREZ', (TO_DATE('01-01-1990', 'DD-MM-YYYY')));
+  IF v_id IS NOT NULL THEN
+    dbms_output.put_line('El autor '||v_id||' se ha dado de alta correctamente');
+  ELSE
+    dbms_output.put_line('El autor no se ha dado de alta');
+  END IF;
+  
+END;
+/
+
+
+select * from autor;
 select * from obra;
+
+ --12. La vinculación de un autor a una obra se implementará mediante una función vincular.
+DECLARE
+  v_resultado INTEGER;
+BEGIN   
+  v_resultado := bibliotk.vincular('GX8S','AX87N');
+ IF v_resultado = 1 THEN
+    dbms_output.put_line('El autor y la obra se han vinculado');
+  ELSE
+    dbms_output.put_line('No se han podido vincular');
+  END IF;
+END;
+/
+
+
+
+select * from autor;
