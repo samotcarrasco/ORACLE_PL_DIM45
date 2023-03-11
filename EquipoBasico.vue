8e7394aaad8e72d951ad29194c11c1a048f3fb85
samotcarrasco <samotcarrasco@gmail.com>
2023-03-11 21:41:19 +0100

    añadidos ficheros vue

---
+++ EquipoBasico.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  props: {
+    nombreEquipo: String,
+    idEquipo: String
+  },
+  emits: ['clickGol', 'clickAmarilla', 'clickRoja'],
+  data() {
+    return {
+      contadorGoles: 0,
+      contadorAmarillas: 0,
+      contadorRojas: 0
+    }
+  },
+  methods: {
+    clickGol() {
+      this.contadorGoles++;
+      this.$emit('clickGol','Gol',this.nombreEquipo)
+    },
+    clickAmarilla() {
+      this.contadorAmarillas++;
+      this.$emit('clickAmarilla','Tarjeta Amarilla',this.nombreEquipo)
+    },
+    clickRoja() {
+      this.contadorRojas++;
+      this.$emit('clickRoja','Tarjeta Roja',this.nombreEquipo)
+    },
+  }
+}
+</script>
+
+<template>
+  <div>
+    <button class="gol" @click="clickGol">¡¡GOL!! </button>
+    <button class="roja" @click="clickAmarilla">ROJA</button>
+    <button class="amarilla" @click="clickRoja">AMARILLA</button>
+
+    <b>{{ idEquipo }} - {{ nombreEquipo }} </b> Goles: <b>{{ contadorGoles }}</b> 
+    Amarillas <b :class="{ amarillas:contadorAmarillas > 0 }">{{ contadorAmarillas }}</b> Rojas <b :class="{ rojas:contadorRojas > 0 }">{{ contadorRojas }}</b>
+    <br>
+  </div>
+</template>
+
+<style>
+.gol {
+  color: green;
+}
+
+.amarilla {
+  color: rgb(221, 221, 48);
+}
+
+.roja {
+  color: red;     
+}
+
+</style>
+        
+    
---
+++ SucesosEquiposBasico.vue
@@ -0,0 +1,68 @@
+<script>
+import EquipoBasico from './EquipoBasico.vue'
+import participantesJSON from '../assets/participantes.json'
+
+export default {
+  components: {
+    EquipoBasico
+  },
+  data() {
+    return {
+      golesTotales: 0,
+      rojasTotales: 0,
+      amarillasTotales: 0,
+      participantes: [],
+      ultimoSuceso: ''
+    }
+  },
+  created() {
+    this.participantes = participantesJSON
+  },
+  methods: {
+    golClickeado(nombreSuceso, equipo) {
+      console.log("GOL RECIBIDO DESDE ELEMENTO HIJO")
+      this.golesTotales++;
+      this.ultimoSuceso = nombreSuceso + " del " + equipo;
+    },
+    amarillaClickeada(nombreSuceso, equipo) {
+      console.log("AMARILLA RECIBIDA DESDE ELEMENTO HIJO")
+      this.amarillasTotales++;
+      this.ultimoSuceso = nombreSuceso + " del " + equipo;
+    },
+    rojaClickeada(nombreSuceso, equipo) {
+      console.log("ROJA RECIBIDA DESDE ELEMENTO HIJO")
+      this.rojasTotales++;
+      this.ultimoSuceso = nombreSuceso + " del " + equipo;
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="contenedor-centrado">
+    <h3>RESULTADOS TOTALES</h3>
+    Goles {{ golesTotales }}
+    Amarillas {{ amarillasTotales }}
+    Rojas {{ rojasTotales }}
+    <hr>
+    <h5> Ultimo suceso: <span> {{ ultimoSuceso }} </span> </h5>
+  </div>
+  <hr>
+  <div class="contenedor-equipos">
+    <EquipoBasico v-for="participante of participantes" :nombreEquipo="participante.nombre" :idEquipo="participante.id"
+      @click-gol="golClickeado" @click-amarilla="amarillaClickeada" @click-roja="rojaClickeada" />
+  </div>
+</template>      
+
+<style>
+.contenedor-centrado {
+  text-align: center;
+  margin: 5vw;
+  margin-top: 56px;
+  margin-bottom: 0px;
+}
+</style>
+
+
+
